03a1fa700c82bc847aede94363b9508d622e23ae
Jaedon-nanohackers <Jsballeyne@optonline.net>
2019-04-14 15:40:01 -0400

    finished filter

--- week1/filterimage.py
+++ week1/filterimage.py
@@ -5,15 +5,22 @@ img = plt.imread("bestwangle.jpeg")
 imgrows, imgcols = img.shape[0], img.shape[1]
 rgb = img.shape[2]
 filtered = nump.zeros((imgrows, imgcols, rgb), dtype=int)
-blur=[0.75,0.65,0.75],[0.65,0.75,0.65],[0.75,0.65,0.75]
+blur=nump.array(([1,2,1],[0,0,0],[-1,-2,-1]))
 
 
 print(blur[0][1])
+holdcell = filtered
+xl = 0
+yl = 0
+for x in range(3,imgrows-3):
+    xl=x+3
+    for y in range(3,imgcols-3):
+        yl = y+3
+        holdcell[x:xl,y:yl] = img[x:xl,y:yl]
+        filtered[x:xl,y:yl] = holdcell[x,y:yl] @ blur
+        # for z in range(2):
+        #     for a in range(0,2):
 
-for x in range(3,imgrows-1):
-    for y in range(3,imgcols-1):
-        for z in range(0,rgb-1):
-            for a in range(0,2):
                 # if a + y < imgcols:
                 #     filtered[x+z,y+a,:] = filtered[x+z,y+a,:]*blur[0][:]
                 # else:
@@ -22,9 +29,9 @@ for x in range(3,imgrows-1):
                 #     filtered[x+z,y+a,:] = filtered[x+z,y+a,:]*blur[0][:]
                 # else:
                 #     filtered[x,y+a,:] = filtered[x,y+a,:]*blur[0][:]
-
-                filtered[x+z,y+a,:] = filtered[x+z,y+a,:]*blur[a][:]
-
+                #
+                # filtered[x+z,y+a,:] = filtered[x+z,y+a,:]*blur[a][:]
 
 plt.imshow(filtered), plt.show()
+filtered = filtered.astype(nump.uint8)
 plt.imsave("C:/Users/Phoenix-Ptah/Desktop/Nanohackers/ml-spring-2019/week1/bestwangleblurry.jpeg", filtered)
